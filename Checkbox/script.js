fb4184082546fbd8e7ee78b2e5f58da4e43fe6e3
Sushant2 <er.sushant.2@gmail.com>
2020-05-27 12:44:02 +0530

    chnaging checkbox selection like gmail

---
+++ Checkbox/script.js
@@ -0,0 +1,28 @@
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+let lastChecked;
+
+function handleCheck(e){
+    //check if they had the shift key down
+    //check that they are checking it
+    let inBetween = false;
+    if(e.shiftKey&& this.checked){
+        //go ahead and do what we please
+        //loop over wvery single checkbox
+        checkboxes.forEach(checkbox => {
+            console.log(checkbox);
+            if(checkbox === this || checkbox === lastChecked){
+                inBetween = !inBetween;
+                console.log('Starting to check them inbetween');
+                
+            }
+            if(inBetween){
+                checkbox.checked = true;
+            }
+        })
+
+    }
+    lastChecked = this;
+}
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
