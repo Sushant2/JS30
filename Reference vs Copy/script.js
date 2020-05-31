86534095e5473ebca5a4ab549c5f968cbca7d82b
Sushant2 <er.sushant.2@gmail.com>
2020-05-31 15:17:08 +0530

    taking to all methods for refrencing & copying

--- Reference vs Copy/script.js
+++ Reference vs Copy/script.js
@@ -1,11 +1,27 @@
 // start with strings, numbers and booleans
 
+// let age = 100;
+// let age2 = age;
+// console.log(age, age2);
+// age = 200;
+// console.log(age, age2);
+
+// let name = 'sushant';
+// let name2 = name;
+// console.log(name, name2);
+// name = 'sushantg';
+// console.log(name, name2);
+
 // Let's say we have an array
 const players = ["Wes", "Sarah", "Ryan", "Poppy"];
 
 // and we want to make a copy of it.
+const team = players;
+
+console.log(players, team);
 
 // You might think we can just do something like this:
+// team[3] = 'Lux';
 
 // however what happens when we update that array?
 
@@ -18,11 +34,17 @@ const players = ["Wes", "Sarah", "Ryan", "Poppy"];
 // So, how do we fix this? We take a copy instead!
 
 // one way
+const team2 = players.slice();
 
 // or create a new array and concat the old one in
-
+const team3 = [].concat(players);
 // or use the new ES6 Spread
+const team4 = [...players];
+team4[3] = 'heeeee hawwww';
+console.log(team4);
 
+
+const team5 = Array.from(players);
 // now when we update it, the original one isn't changed
 
 // The same thing goes for objects, let's say we have a person object
@@ -34,9 +56,22 @@ const person = {
 };
 
 // and think we make a copy:
+// const captain = person;
+// captain.number = 99;
 
 // how do we take a copy instead?
+const cap2 = Object.assign({}, person, { number: 99, age: 12});
+console.log(cap2);
 
 // We will hopefully soon see the object ...spread
 
-// Things to note - this is only 1 level deep - both for Arrays and Objects. lodash has a cloneDeep method, but you
+const cap3 = {...person};
+
+// Things to note - this is only 1 level deep - both for Arrays and Objects. lodash has a cloneDeep method, but you should think twice before using it
+
+
+const susu = {
+    name: 'sushant',
+    age: 19,
+    social 
+}
