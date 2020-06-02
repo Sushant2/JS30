8b30ae4f99405dac6c586646514211c3730cd18a
Sushant2 <er.sushant.2@gmail.com>
2020-06-02 22:26:26 +0530

    taking item data

--- Storage-Event Delegation/script.js
+++ Storage-Event Delegation/script.js
@@ -1,3 +1,16 @@
 const addItems = document.querySelector(".add-items");
 const itemsList = document.querySelector(".plates");
 const items = [];
+
+function addItem(e){
+    e.preventDefault();
+    const text = this.querySelector('[name=item]');
+    const item = {
+        text: 'Item Name',
+        done: false
+    }
+    
+}
+
+
+addItems.addEventListener('submit', addItem);
